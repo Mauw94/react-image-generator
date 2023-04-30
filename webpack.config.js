55a561f8d361a8ba5246be3121b995ea3b42174d
Maurits <mauritsseelen@hotmail.com>
2023-04-30 22:33:03 +0200

    cp index html to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 "use strict";
 
-const path = require("path");
 const webpack = require('webpack')
 const dotenv = require('dotenv')
+const CopyPlugin = require("copy-webpack-plugin")
 
 dotenv.config();
 
 module.exports = {
   plugins: [
-    // ...
     new webpack.DefinePlugin({
       'process.env': JSON.stringify(process.env)
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "index.html", to: "index.html" },
+      ],
     })
-    // ...
   ],
 
   // Set debugging source maps to be "inline" for
@@ -20,7 +23,7 @@ module.exports = {
   devtool: "inline-source-map",
 
   // The application entry point
-  entry: "./src/index.tsx",
+  entry: ["./src/index.tsx"],
 
   // Where to compile the bundle
   // By default the output directory is `dist`
